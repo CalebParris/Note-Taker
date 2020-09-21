d8fb7eee006077a6a4b7a0102b773772e023ee2a
Caleb Parris <calebparris@live.com>
2020-09-21 14:46:25 -0500

    Server displays HTML pages

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const fs = require('fs');
-const express = require('express');
-const path = require('path');
+// Dependencies
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 
+// Express setup
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 8080;
-
+// Data parsing for Express
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json);
+app.use(express.json());
 
-//=============================
+// HTML Routes
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
-// Write server code here
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-//=============================
+// API Routes
 
+// Server listens on the port when started
 app.listen(PORT, function(){
-    console.log('App listening on PORT: ' + PORT);
-})
+    console.log("App listening on PORT: " + PORT);
+});
